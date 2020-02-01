85f2d34f06292a4e605e5d104e41c3f22fbaffbb
Jarek Lupinski <jarek@lupin.ski>
2020-02-01 14:42:58 -0500

    db init

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,26 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'DUMMYUSER'}
-    return render_template('index.html', title='GreyFinder', user=user)
+    pictures = [
+        {
+            'url': 'https://i.picsum.photos/id/12/300/200.jpg'
+        },
+        {
+            'url': 'https://i.picsum.photos/id/434/300/200.jpg'
+        }
+    ]
+    return render_template('index.html', title='GreyFinder', user=user, pictures=pictures)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
